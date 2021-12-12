c5bb27c281b30b63808feecffeb2eef642332d7b
Jakob Everding <jakob.everding@web.de>
2021-12-12 16:28:12 +0100

    added inner_execute function called inside run_selenium

--- src/rotate.py
+++ src/rotate.py
@@ -93,14 +93,64 @@ def run_selenium():
             print(f"Proxy position in current pool, n_proxy: {n_proxy}; pool size: {len(proxy_pool)}")
             proxy = next(proxy_iter)
             driver = driver_proxy(proxy=proxy)
-            # TODO (JE): Include code/script to execute scraping here
-            driver.quit()
+
+            inner_execute(driver=driver)
+
+            if driver:
+                driver.quit()
         except Exception as e:
             print(e)
             print(f"Trying with other proxy; n_proxy: {n_proxy}")
             n_proxy += 1
             continue
         break
+
+
+def inner_execute(driver):
+    driver.get("https://www.lbv-termine.de/frontend/onlinedienstleistung.php?dienstleistungsid=176")
+
+    # Bug:
+    dates = driver.find_elements(by=By.NAME, value="daten")
+    dates = str(dates)
+
+    # Site: Data protection
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "datenschutzgelesen"))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "weiterbutton"))).click()
+
+    # Site: Personal data
+    fname_box = driver.find_element(by=By.ID, value="vorname")
+    fname_box.send_keys(first_name)
+    lname_box = driver.find_element(by=By.ID, value="nachname")
+    lname_box.send_keys(last_name)
+    email_box = driver.find_element(by=By.ID, value="email")
+    email_box.send_keys(email)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "weiterbutton"))).click()
+
+    # Site: Site selection
+    x = driver.find_element(by=By.CSS_SELECTOR, value="a[href*='terminauswahl.php?standortid=109']")
+    x.click()
+
+    # Site: Date selection
+    dates = driver.find_elements(by=By.NAME, value="daten")
+    min_date_str = dates[0].get_attribute("id")
+    # Proceed only if earliest available date is before some specified date
+    min_date = datetime.strptime(min_date_str, "%Y-%m-%d")
+    if min_date < datetime(2021, 12, 22):
+        x = driver.find_element(by=By.ID, value=min_date_str)
+        x.click()
+
+        times = driver.find_elements(by=By.NAME, value="zeiten")
+        min_time = times[0].get_attribute("id")
+        driver.find_element(by=By.ID, value=min_time).click()
+
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "weiterbutton"))).click()
+
+        # End
+        print("Appointment booked, end")
+
+    else:
+        # End
+        print("No appointment booked, end")
 
 
 def main():
